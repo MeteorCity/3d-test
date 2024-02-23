91b9a04b06805c0495471629ace1b1bd04fd4e66
unknown <john.talghader@gmail.com>
2024-02-23 01:11:04 -0600

    added shoe model

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,16 +1,16 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react';
-import Donut from '../public/Donut'
+import Shoe from '../public/Shoe'
 
 const Model = () => {
     return (
         <div className="model">
             <Canvas>
                 <ambientLight />
-                <OrbitControls maxDistance={12} minDistance={3} enablePan={false} /* enableDamping={false} *//>
+                <OrbitControls maxDistance={100} minDistance={3} enablePan={false} /* enableDamping={false} *//>
                 <Suspense fallback={null}>
-                    <Donut />
+                    <Shoe />
                 </Suspense>
             </Canvas>
         </div>
